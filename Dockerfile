4f003c9e1dc5d3c5560749ecd1b2b00078b92fde
Luis Felipe Elgueta <luis.elgueta@trebol-it.cl>
2020-02-14 10:22:19 -0300

    ajustes

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
 
-#Specify a base image
 FROM node:alpine
 
-#Specify a working directory
 WORKDIR /usr/app
 
-#Copy the dependencies file
 COPY ./package.json ./
 
-#Install dependencies
 RUN npm install 
 
-#Copy remaining files
 COPY ./ ./
 
-#Default command
+EXPOSE 3000:3000
+
 CMD ["npm","start"]
--- core/index.js
+++ core/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const cors = require('cors');
+const bodyparser = require("body-parser");
 const app = express();
 const rutas = require("./routes");
-const puerto = process.env.APP_PORT;
+
+const puerto = process.env.APP_PORT || 3000;
 
 app.use(rutas);
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+app.use(bodyparser.json());
+
 
 app.listen(puerto, () => {
     console.log(`Iniciando servidor en puerto ${puerto}`);
