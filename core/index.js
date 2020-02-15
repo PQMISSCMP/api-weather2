7e5445b4055fb1e7a0cf726f387051329cd32cfc
Luis Felipe Elgueta <luis.elgueta@trebol-it.cl>
2020-02-14 22:45:08 -0300

    cors

--- core/index.js
+++ core/index.js
@@ -1,23 +1,10 @@
 const express = require("express");
-const cors = require('cors');
-const bodyparser = require("body-parser");
 const app = express();
 const rutas = require("./routes");
 
-const puerto = process.env.APP_PORT || 3000;
-
+const puerto = process.env.APP_PORT || 8000;
 app.use(rutas);
 
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-
-app.use(bodyparser.json());
-
-
 app.listen(puerto, () => {
-    console.log(`Iniciando servidor en puerto ${puerto}`);
+    console.log(`7-Iniciando servidor en puerto ${puerto}`);
 });
