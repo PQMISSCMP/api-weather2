ae55805cfb9465d4db51a48947f194031f456db8
Luis Felipe Elgueta <luis.elgueta@trebol-it.cl>
2020-02-16 02:06:25 -0300

    OK  heroku ;)

--- core/index.js
+++ core/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const rutas = require("./routes");
 
-const puerto = process.env.APP_PORT || 8000;
+const puerto = (process.env.PORT || 8000);
 app.use(rutas);
 
 app.listen(puerto, () => {
-    console.log(`7-Iniciando servidor en puerto ${puerto}`);
+    console.log(`9-Iniciando servidor en puerto interno ${puerto}`);
 });
